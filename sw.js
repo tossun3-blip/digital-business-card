97ca4ea131f0834e145db25797aeb52aebc42665
tossun3-blip <tossun3@gmail.com>
2026-08-09 10:17:34 +0900

    Add installable PWA support

---
+++ sw.js
@@ -0,0 +1,6 @@
+const CACHE = 'my-card-v1';
+const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
+
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
+self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request))));
